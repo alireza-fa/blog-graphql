54e59ad399b2fe56d2b6fb1c26e4b198fed20a80
alireza-fa <alirezafeyze44@gmail.com>
2025-08-11 21:00:40 +0330

    build: Dockerfile & docker compose

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.13-slim as builder
+
+LABEL name="alireza feizi" email="alirezafeyze44@gmail.com"
+LABEL "website.name"="syntaxfa"
+LABEL website="syntaxfa.com"
+LABEL desc="This is a python Dockerfile for django projects"
+
+# avoid stuck build due to user prompt
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && apt-get install -y --no-install-recommends && apt-get install -y gettext
+
+WORKDIR /code
+COPY . .
+
+RUN pip3 install --upgrade --no-cache-dir pip
+RUN pip3 install --no-cache-dir wheel
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+ENV PYTHONUNBUFFERED=1
---
+++ entrypoint.py
@@ -0,0 +1,23 @@
+"""
+    Gunicorn should only need 4-12 worker processes to handle hundreds or thousands of requests per second.
+    Gunicorn relies on the operating system to provide all of the load balancing when handling requests.
+    Generally we recommend (2 x $num_cores) + 1 as the number of workers to start off with.
+    
+    docs: https://docs.gunicorn.org/en/latest/design.html
+"""
+import os
+
+cpu_count = os.cpu_count()
+
+print(f"cpu count is {cpu_count}")
+
+if cpu_count == 1:
+    WORKERS = 1
+else:
+    WORKERS = 1
+
+
+# running django
+os.system("python3 manage.py migrate --no-input")
+os.system("python3 manage.py collectstatic --no-input")
+os.system("python manage.py runserver 8000")
